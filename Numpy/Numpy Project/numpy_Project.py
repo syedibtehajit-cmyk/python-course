c739e3b241332f2b75df685293cf382decc7e921
Syed Ibtehaj Ishtiaque <syed.ibtehaj.it@gmail.com>
2026-09-03 21:20:31 +0500

    add NUMPY project

--- Numpy/Numpy Project/numpy_Project.py
+++ Numpy/Numpy Project/numpy_Project.py
@@ -20,13 +20,16 @@ student_Highest_Marks = np.max(marks)
 # min marks
 student_Min_Marks = np.min(marks) 
 # subject wise average
-subject_totals_average = np.average(marks, axis=1)
-
+subject_totals_average = np.average(marks, axis=0)
+#best student match
+#argmax() highest value ka index deta hai.
+best_student_index = np.argmax(student_totals)
 print("Each student's total",student_totals)
 print("Each student's average",student_totals_average)
 print("Highest mark",student_Highest_Marks)
 print("Minimum mark",student_Min_Marks)
 print("Subject-wise average",subject_totals_average)
+print("Best student ",best_student_index)
 
 
 #Shape: (4, 3)
